81b9800eb713c14e8dd8e30ddba4dc6fa8f4cae6
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-11 17:00:18 -0300

    Update ex02.py

---
+++ ex02.py
@@ -0,0 +1,22 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 11/07/2022
+##
+# https://super.abril.com.br/mundo-estranho/o-que-e-a-sequencia-de-fibonacci/
+#
+# Os números de Fibonacci são, portanto, os números que compõem a seguinte sequência:
+# 0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, ...
+
+#########################################################
+# fibonacci sem recursão
+#
+#########################################################
+
+# entrada de dados -> número de termos da sequencia de Fibonacci
+termo = int(input("Digite o número de termos da sequencia de Fibonacci: "))
+
+
