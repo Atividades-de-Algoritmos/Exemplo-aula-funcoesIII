5a77774a1f177b8596bb359f1c5ab0d69125ee8f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-11 15:32:19 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -11,13 +11,38 @@
 # Os números de Fibonacci são, portanto, os números que compõem a seguinte sequência:
 # 0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, ...
 
-# termo -> valor
+# termo -> valor do termo atual da sequencia de Fibonacci (número atual)
 # 1 -> 0
-# 2 -> 1
+# 2 -> (1 + 0) -> 1
 # 3 -> 1 + 0 = 1
 # 4 -> 1 + 1 = 2
 # 5 -> 2 + 1 = 3
 # 6 -> 3 + 2 = 5
 # 7 -> 5 + 3 = 8
 # termo n -> termo n-1 + termo n-2 = ...
-# fib(n) -> fib(n-1) + fin(n-2)
+# fib(n) -> fib(n-1) + fin(n-2) + ...
+
+# entrada de dados -> número de termos da sequencia de Fibonacci
+termo = int(input("Digite o número de termos da sequencia de Fibonacci: "))
+
+#
+#############################################################################################################################
+print('#############################################')
+print("#                parte 1                    #")
+print("#############################################")
+#
+'''
+# aplicando a fomula
+def fib(termo):
+  return fib(termo-1) + fib(termo-2)
+
+print(f"fib({termo}) = {fib(termo)}")
+###########################################
+# gera o seguinte erro
+#    return fib(termo-1) + fib(termo-2)
+#  [Previous line repeated 996 more times]
+# RecursionError: maximum recursion depth exceeded
+###########################################
+'''
+
+
