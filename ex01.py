ebe0d4cbd78cf1bb8b0a190205287ce1c034ea8f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-11 15:21:11 -0300

    Update ex01.py

---
+++ ex01.py
@@ -0,0 +1,23 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 11/07/2022
+##
+# https://super.abril.com.br/mundo-estranho/o-que-e-a-sequencia-de-fibonacci/
+#
+# Os números de Fibonacci são, portanto, os números que compõem a seguinte sequência:
+# 0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, ...
+
+# termo -> valor
+# 1 -> 0
+# 2 -> 1
+# 3 -> 1 + 0 = 1
+# 4 -> 1 + 1 = 2
+# 5 -> 2 + 1 = 3
+# 6 -> 3 + 2 = 5
+# 7 -> 5 + 3 = 8
+# termo n -> termo n-1 + termo n-2 = ...
+# fib(n) -> fib(n-1) + fin(n-2)
