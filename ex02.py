9c6e7a73ab57bada2b78f3ada9d8180f6a9457d5
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-11 17:13:45 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -19,4 +19,40 @@
 # entrada de dados -> número de termos da sequencia de Fibonacci
 termo = int(input("Digite o número de termos da sequencia de Fibonacci: "))
 
+#
+#############################################################################################################################
+print('#############################################')
+print("#                parte 1                    #")
+print("#############################################")
+#
 
+def fib1(termo): # escreve a série de Fibonacci até o termo n-ésimo
+  a, b = 0, 1 # inicializa a série de Fibonacci com 0 e 1 como primeiros termos
+  while termo > 0: # enquanto o termo não for atingido
+    print(b, end=' ') # imprime o termo atual da série
+    a, b = b, a + b # atualiza os termos da série de Fibonacci
+    termo -= 1  # decrementa o termo
+
+# saida de dados -> série de Fibonacci
+print("Série de Fibonacci:")
+fib1(termo)
+print("--------------------------------------------")
+#
+#############################################################################################################################
+print('#############################################')
+print("#                parte 2                    #")
+print("#############################################")
+#
+
+def fib2(termo):
+  resultado = []
+  a, b = 0, 1
+  while termo > 0:
+    resultado.append(b)
+    a, b = b, a + b
+    termo -= 1  # decrementa o termo
+  return resultado
+
+# saida de dados -> série de Fibonacci
+print("Série de Fibonacci:")
+print(f"fib({termo}) = {fib2(termo)}", end='')
