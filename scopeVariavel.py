150bf183bdcef9049b9628d5ed84089e72fdfe40
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-12 17:56:46 -0300

    Create scopeVariavel.py

---
+++ scopeVariavel.py
@@ -0,0 +1,28 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 11/07/2022
+
+########################
+# Scorpo das variáveis:#
+########################
+# Todas as variáveis em um programa podem não estar acessíveis em todos os locais desse programa.
+# Isso depende de onde você declarou uma variável.
+
+# O escopo de uma variável determina a parte do programa onde você pode acessar um determinado
+# identificador. Existem dois escopos básicos de variáveis em Python:
+# Variáveis globais
+# Variáveis locais
+
+###############################
+# Variáveis globais vs. locais:
+# As variáveis que são definidas dentro de um corpo de função têm um escopo local e
+# aquelas definidas fora têm um escopo global.
+
+# Isso significa que as variáveis locais podem ser acessadas apenas dentro da função em que são declaradas,
+# enquanto as variáveis globais podem ser acessadas em todo o corpo do programa por todas as funções.
+#  Quando você chama uma função, as variáveis declaradas dentro dela são trazidas para o escopo.
+
