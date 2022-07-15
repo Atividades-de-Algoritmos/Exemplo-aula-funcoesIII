e2dc9a7186cf654969de453f31a1383acfe18c5e
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-15 15:15:27 -0300

    Create funcaoAnonimas.py

---
+++ funcaoAnonimas.py
@@ -0,0 +1,15 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 11/07/2022
+
+########################
+# Funções anônimas:    #
+########################
+
+# O que é uma função lambda?
+# Um lambda é simplesmente uma maneira de definir uma função em Python. Às vezes são conhecidos como
+# operadores lambda ou funções lambda.
