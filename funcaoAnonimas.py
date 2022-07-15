2678fa5d832b5f2a2bf0328fa7a66d6e1c4f768b
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-15 15:16:28 -0300

    Update funcaoAnonimas.py

--- funcaoAnonimas.py
+++ funcaoAnonimas.py
@@ -13,3 +13,14 @@
 # O que é uma função lambda?
 # Um lambda é simplesmente uma maneira de definir uma função em Python. Às vezes são conhecidos como
 # operadores lambda ou funções lambda.
+
+#############################################################################################################################
+print('#############################################')
+print("#                 parte 1:                  #")
+print("#############################################")
+## vamos ver uma função super básica definida da maneira “tradicional”: Aqui está uma
+# função simples que duplica o valor passado.
+def dobro1(x):  # função que duplica um número
+  return x * 2  # retorna o dobro do número
+
+print(dobro1(2)) # imprime 4
